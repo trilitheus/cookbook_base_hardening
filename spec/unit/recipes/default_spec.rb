e80d69e9b8460dc9d5073e87c29111ae93a19ba8
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-07-14 11:23:58 +0100

    Update default recipe spec

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,14 +7,25 @@
 require 'spec_helper'
 
 describe 'base_hardening::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  context 'When all attributes are default on RHEL family' do
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new
+    runner.node.automatic['os'] = 'linux'
+    runner.node.automatic['platform_family'] = 'rhel'
+    runner.converge(described_recipe)
+  end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
+  end
+
+  it 'includes the openssh recipe' do
+    expect(chef_run).to include_recipe('openssh')
+  end
+
+  it 'includes the rhel recipe' do
+    expect(chef_run).to include_recipe('base_hardening::rhel')
+  end
+
   end
 end
