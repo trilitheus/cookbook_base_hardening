b8b1a8ff559e61517629646c326da949b415f5b1
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-07-14 11:40:05 +0100

    Add basic spec for rhel recipe

---
+++ spec/unit/recipes/rhel_spec.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: base_hardening
+# Spec:: rhel
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'base_hardening::rhel' do
+  context 'When all attributes are default on RHEL family' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.node.automatic['os'] = 'linux'
+      runner.node.automatic['platform_family'] = 'rhel'
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+  end
+end
