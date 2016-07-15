d1324f8910f77fcd6326623faf4bf6e57f07d975
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-07-15 17:10:10 +0100

    Bump to version 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures base_hardening'
 long_description 'Installs/Configures base_hardening'
-version '0.1.0'
+version '0.2.0'
 
 depends 'openssh', '~> 2.0.0'
