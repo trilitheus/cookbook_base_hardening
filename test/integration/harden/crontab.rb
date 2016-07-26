b6f87c81203800c405340d9adb55cc3ebdd942f1
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-07-26 16:48:20 +0100

    Remove duplicate tests

--- test/integration/harden/crontab.rb
+++ test/integration/harden/crontab.rb
@@ -119,35 +119,6 @@ describe file('/etc/cron.d') do
   its('mode') { should eq 0700 }
 end
 
-describe file('/etc/profile') do
-  its('content') { should match(/umask\s+022/) }
-end
-
-describe file('/etc/sysconfig/init') do
-  its('content') { should match(/umask\s+027/) }
-end
-
-describe file('/etc/bashrc') do
-  its('content') { should match(/umask\s+022/) }
-end
-
-describe file('/etc/modprobe.d/CIS.conf') do
-  its('content') { should match(%r{install\s+cramfs\s+/bin/true}) }
-end
-describe file('/etc/modprobe.d/CIS.conf') do
-  its('content') { should match(%r{install\s+squashfs\s+/bin/true}) }
-end
-describe file('/etc/modprobe.d/CIS.conf') do
-  its('content') { should match(%r{install\s+udf\s+/bin/true}) }
-end
-
-describe file('/etc/sysctl.conf') do
-  its('content') { should match(/kernel.randomize_va_space\s+=\s+2/) }
-end
-describe file('/etc/sysctl.conf') do
-  its('content') { should match(/fs.suid_dumpable\s+=\s+0/) }
-end
-
 describe file('/etc/crontab') do
   its('owner') { should eq 'root' }
 end
