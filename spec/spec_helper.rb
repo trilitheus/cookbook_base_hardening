13cdae6fe844e3d3373380de500bd303a571bde4
Roy Crombleholme <roy.crombleholme@pam-it.com>
2017-01-26 11:41:34 +0000

    [ChefSpec] Refactor platform into spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,17 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+RSpec.configure do |config|
+  config.platform = 'centos'
+  config.version = '7.2.1511'
+  config.tty = true
+  config.color = true
+  config.log_level = :error
+  config.formatter = :documentation
+  config.run_all_when_everything_filtered = true
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
 at_exit { ChefSpec::Coverage.report! }
