254ca55a838fad47dbd973cfd644f3796755b510
Roy Crombleholme <roy.crombleholme@pam-it.com>
2017-02-24 23:47:15 +0000

    Bump version add supports

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name 'base_hardening'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Alliantist Ltd'
+maintainer_email 'sysadmins@pam-it.com'
 license 'all_rights'
-description 'Installs/Configures base_hardening'
-long_description 'Installs/Configures base_hardening'
-version '0.3.1'
+description 'Configures various elements of a server in accordance with CIS recommendations'
+long_description 'Configures various elements of a server in accordance with CIS recommendations'
+version '0.3.2'
+
+supports 'redhat', '= 7'
+supports 'centos', '= 7'
 
 depends 'openssh', '~> 2.0.0'
 depends 'sysctl', '~> 0.8.0'
